dc756a63b81650e5202fdc2960eb4c0371468308
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2026-01-01 21:04:33 +1000

    adding staggering children effect

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -3,7 +3,7 @@ import { useContext, useRef, useState } from 'react';
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import Modal from './Modal.jsx';
 import images from '../assets/images.js';
-import { motion } from 'framer-motion';
+import { motion, stagger } from 'framer-motion';
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
@@ -57,7 +57,12 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul 
+          id="new-challenge-images"
+          variants={{ 
+            visible: { transition: { staggerChildren: 0.1 } }
+          }}
+        >
           {images.map((image) => (
             <motion.li
               variants={{
@@ -73,7 +78,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
