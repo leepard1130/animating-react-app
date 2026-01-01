fafd5ae0848e20600b3c0ec025bff5be55aa91dd
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2026-01-01 22:04:56 +1000

    multi elements animation

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from 'react';
-
+import { AnimatePresence, motion } from 'framer-motion';
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import ChallengeItem from './ChallengeItem.jsx';
 import ChallengeTabs from './ChallengeTabs.jsx';
@@ -40,19 +40,30 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+        <AnimatePresence>
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+            key="list"
+              exit ={{y:-30, opacity:0 }}
+              className="challenge-items">
+            <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                    <ChallengeItem
+                      key={challenge.id}
+                      challenge={challenge}
+                      onViewDetails={() => handleViewDetails(challenge.id)}
+                      isExpanded={expanded === challenge.id}
+                    />
+                ))}
+              </AnimatePresence>            
+            </motion.ol>
+          )}
+          {displayedChallenges.length === 0 && <motion.p 
+                                                key="fallback"
+                                              >
+                                                No challenges found.
+                                              </motion.p>}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
