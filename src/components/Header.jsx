63eea420700b189843cb76c8017032eacbd63f28
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2026-01-01 20:35:46 +1000

    adding pop effect while hovering the button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 import NewChallenge from './NewChallenge.jsx';
 
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -23,9 +23,13 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 300 }}
+          onClick={handleStartAddNewChallenge} 
+          className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
